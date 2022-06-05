ef85a1ddeee315520a99899960cd1e76a1f3b9dd
unknown <johnferreira141@gmail.com>
2022-06-05 18:36:22 -0300

    Introdução ao JavaScript

---
+++ JavaScript/Introdução ao JavaScript/RecursosBasicos/ManipulandoArquivos.js
@@ -0,0 +1,18 @@
+//Comentario de linha
+
+/*Comentario
+varias
+linhas */
+
+//Variaveis var: Os valores podem mudar no codigo, variavel dinamica
+//Constantes Const: valor não pode ser mudado, "variavel" estatica
+
+var preco = 2;
+var desc = 0.2;
+
+const PRECO2=2
+var total = PRECO2 - desconto;
+
+preco = preco - desc;
+
+total = PRECO2 - desc;
---
+++ JavaScript/Introdução ao JavaScript/RecursosBasicos/funções.js
@@ -0,0 +1,5 @@
+function soma(a, b) {//Declaração de função simples
+    return a + b;
+}
+
+console.log(soma(5, 6));//Mostrando a função no console com os valores de a=5 e b=6
